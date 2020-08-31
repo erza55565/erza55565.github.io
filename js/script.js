c45e680196ae033c01bcbb973a0e4e4155b666d6
Koval Yelyzaveta <elizaveta55565@gmail.com>
2020-08-31 20:12:50 +0300

    merge contact-form and footer, change contact-form color, improve contact-form, change cards markup

--- js/script.js
+++ js/script.js
@@ -1,191 +1,197 @@
 let cardsArray = [{
         'headerTitle': 'Airlines online booking',
         'header': 'Implementation of a couple microservices for the airlines booking system. Booking tickets, processing flights, routing, prices, discounts, etc. Also, the project had the internal layers of system services for big data processing such as recalculation prices, flight shifting, optimization, etc. ',
-        'body': 'Team: 3 backend, 2 frontend, 1 devops. ' +
-            'Language: Java / Scala' +
-            'Technologies: Docker, Spark, RabbitMQ, Cassandra ' +
-            'Platform: Cross-platform' +
-            'Role: Architect / Core developer ',
-        'footerList': 'Architecture.' +
-            'BackEnd core features development.' +
-            'Technical consultations.' +
-            'Troubleshooting. ' +
-            'Interviewing new employers. ' +
+        'body': '<b>Team</b> 3 backend, 2 frontend, 1 devops.<br>' +
+            '<b>Language</b> Java / Scala<br>' +
+            '<b>Technologies</b> Docker, Spark, RabbitMQ, Cassandra<br>' +
+            '<b>Platform</b> Cross-platform<br>' +
+            '<b>Role</b> Architect / Core developer ',
+        'footerList': 'Architecture.<br>' +
+            'BackEnd core features development.<br>' +
+            'Technical consultations.<br>' +
+            'Troubleshooting.<br>' +
+            'Interviewing new employers.<br>' +
             'Solving difficult technical issues ',
         'footerResult': 'The customer has improved speed of transaction processing (booking, searching, etc) Optimization of logistics and fees has been implemented. '
     },
     {
         'headerTitle': 'Payment banking based on blockchain infrastructure',
         'header': 'Research and investigation which blockchain network will be the best for specific payment transaction. The main idea is comparing, analyze, and select appropriate blockchain network for the specific business transaction. The goal was transaction size, time and confirmation.',
-        'body': 'Team: 3 backend, 3 frontend, 2 devops. ' +
-            'MoreInfo: https://github.com/imaxfp/blockchain-workbench-rest' +
-            'Language: Java / Scala  ' +
-            'Technologies: Docker, r3Corda, Hyperledger, Ethereum.  ' +
-            'Platform: Cross-platform' +
-            'Role: Architect / Core developer',
-        'footerList': 'Architecture.' +
-            'BackEnd core features development.' +
-            'Technical consultations.' +
-            'Troubleshooting. ' +
-            'Interviewing new employers. ' +
+        'body': '<b>Team</b> 3 backend, 3 frontend, 2 devops.<br>' +
+            '<b>MoreInfo</b> <a href="https://github.com/imaxfp/blockchain-workbench-rest">https://github.com/imaxfp/blockchain-workbench-rest</a> <br>' +
+            '<b>Language</b> Java / Scala<br>' +
+            '<b>Technologies</b> Docker, r3Corda, Hyperledger, Ethereum.<br>' +
+            '<b>Platform</b> Cross-platform<br>' +
+            '<b>Role</b> Architect / Core developer',
+        'footerList': 'Architecture.<br>' +
+            'BackEnd core features development.<br>' +
+            'Technical consultations.<br>' +
+            'Troubleshooting.<br>' +
+            'Interviewing new employers.<br>' +
             'Solving difficult technical issues ',
         'footerResult': 'The POC has been launched. Customer tested transaction and selected appropriate technology for the production product  '
     },
     {
         'headerTitle': 'Supply chain solution for cargo delivery',
         'header': 'The main idea of the project is solving trust issue between businesses and merchants. Exclude human errors and proxies agents. Speed up transaction processing. ',
-        'body': 'Team: 5 backend, 4 frontend, 2 devops. ' +
-            'Language: Scala  ' +
-            'Technologies: Docker, r3Corda' +
-            'Platform: Cross-platform' +
-            'Role: Architect / Core developer',
-        'footerList': 'Architecture.' +
-            'BackEnd core features development.' +
-            'Technical consultations.' +
-            'Troubleshooting. ' +
-            'Interviewing new employers. ' +
+        'body': '<b>Team</b> 5 backend, 4 frontend, 2 devops.<br>' +
+            '<b>Language</b> Scala<br>' +
+            '<b>Technologies</b> Docker, r3Corda<br>' +
+            '<b>Platform</b> Cross-platform<br>' +
+            '<b>Role</b> Architect / Core developer',
+        'footerList': 'Architecture.<br>' +
+            'BackEnd core features development.<br>' +
+            'Technical consultations.<br>' +
+            'Troubleshooting.<br>' +
+            'Interviewing new employers.<br>' +
             'Solving difficult technical issues ',
         'footerResult': 'The project was launched in the production mode. Customer solved the issue with trust between different counterparties. Excluded agents and counterparties. Saved money and time. '
     }, {
         'headerTitle': 'Prediction and searching financial instruments based on ML/AI algorithms',
         'header': 'ML based solution for searching similarities of different trading securities, bonds, etc. The main goal of the project - analyzing, classification and select closest financial instruments by the selected features. The project use mixed approach wit supervised and unsupervised learning. ',
-        'body': 'Team: 4 backend, 2 frontend, 1 devops. ' +
-            'Language: Python, Java   ' +
-            'Technologies and libs: https://matplotlib.org,  https://scikit-learn.org, https://www.docker.com, https://pandas.pydata.org, ' +
-            'Platform: Cross-platform' +
-            'Role: Data scientist / Core developer ',
-        'footerList': 'Data analyzing.  ' +
-            'Architecture.' +
-            'BackEnd core features development.' +
-            'Technical consultations.' +
+        'body': '<b>Team</b> 4 backend, 2 frontend, 1 devops.<br>' +
+            '<b>Language</b> Python, Java<br>' +
+            'Technologies and libs: <a href="https://matplotlib.org"></a>,  https://scikit-learn.org, https://www.docker.com, https://pandas.pydata.org"></a><br>' +
+            '<b>Platform</b> Cross-platform<br>' +
+            '<b>Role</b> Data scientist / Core developer ',
+        'footerList': 'Data analyzing.<br>' +
+            'Architecture.<br>' +
+            'BackEnd core features development.<br>' +
+            'Technical consultations.<br>' +
             'Troubleshooting. ',
         'footerResult': 'Project provides pipeline for analyzing financial instruments. Finding similarities by custom selected features on the big market data sets. Financial portfolio optimization and allocation. Determination of perspective financial instrument by the predefined policies and features.'
     }, {
         'headerTitle': 'ML/AI based solution for prediction of volatility financial instruments',
         'header': 'The main idea is tracking selected financial indexes and features. Provide unsupervised cauterization algorithms and supervised learning based on big financial datasets.',
-        'body': 'Team: 4 backend, 2 frontend, 1 devops. ' +
-            'Language: Python, Java   ' +
-            'Technologies and libs: https://matplotlib.org,  https://scikit-learn.org, https://www.docker.com, https://pandas.pydata.org, ' +
-            'Platform: Cross-platform' +
-            'Role: Data scientist / Core developer ',
-        'footerList': 'Data analyzing.  ' +
-            'Architecture.' +
-            'BackEnd core features development.' +
-            'Technical consultations.' +
+        'body': '<b>Team</b> 4 backend, 2 frontend, 1 devops.<br>' +
+            '<b>Language</b> Python, Java<br>' +
+            'Technologies and libs: https://matplotlib.org,  https://scikit-learn.org, https://www.docker.com, https://pandas.pydata.org,<br>' +
+            '<b>Platform</b> Cross-platform<br>' +
+            '<b>Role</b> Data scientist / Core developer ',
+        'footerList': 'Data analyzing.<br>' +
+            'Architecture.<br>' +
+            'BackEnd core features development.<br>' +
+            'Technical consultations.<br>' +
             'Troubleshooting. ',
         'footerResult': 'Traders and investors have got ML/AI helper for double-checking their hypothesis. Risk optimization, searching profitable trading strategies and perspective financial instruments.'
     }, {
         'headerTitle': 'The project was connected with financial and trading industry',
         'header': 'The main idea is clusterization and finding similarities between trading stocks.',
-        'body': 'Team: 5 backend, 4 frontend, 2 devops. ' +
-            'Language: Python / Java    ' +
-            'Technologies: Docker, spring boot, hibernate,  sklearn, pandas, matplotlib, etc' +
-            'Platform: Cross-platform' +
-            'Role: Architect / Core developer ',
-        'footerList': 'Architecture and development.  ' +
-            'Data analyzes.' +
-            'Applying ML algorithms. ' +
-            'Training models.' +
+        'body': '<b>Team</b> 5 backend, 4 frontend, 2 devops.<br>' +
+            '<b>Language</b> Python / Java<br>' +
+            '<b>Technologies</b> Docker, spring boot, hibernate,  sklearn, pandas, matplotlib, etc<br>' +
+            '<b>Platform</b> Cross-platform<br>' +
+            '<b>Role</b> Architect / Core developer ',
+        'footerList': 'Architecture and development.<br>' +
+            'Data analyzes.<br>' +
+            'Applying ML algorithms.<br>' +
+            'Training models.<br>' +
             'Troubleshooting. ',
         'footerResult': 'Pipeline for processing and prediction stocks was launched. Customer has got fast tool for researching and finding similarities of stocks by custom features. '
     }, {
         'headerTitle': '',
         'header': '',
-        'body': 'Language: Scala  ' +
-            'Technologies: r3Corda, Java, Docker, Postgres, Akka, Spring, ReactJS' +
-            'Platform: Cross-platform' +
-            'Role: CTO ',
-        'footerList': 'Architecture,' +
-            ' BackEnd, FrontEnd, Infrastructure organization' +
-            ' Saving human resources(avoiding burnout in fast-changeable environment with possibility to work under pressure, etc),' +
-            ' Saving costs,' +
-            ' Interviewing new employers,' +
-            ' Long-term strategy of delivery,' +
-            ' Communicating with investor representatives,' +
+        'body': '<b>Language</b> Scala<br>' +
+            '<b>Technologies</b> r3Corda, Java, Docker, Postgres, Akka, Spring, ReactJS<br>' +
+            '<b>Platform</b> Cross-platform<br>' +
+            '<b>Role</b> CTO ',
+        'footerList': 'Architecture,<br>' +
+            ' BackEnd, FrontEnd, Infrastructure organization<br>' +
+            ' Saving human resources(avoiding burnout in fast-changeable environment with possibility to work under pressure, etc),<br>' +
+            ' Saving costs,<br>' +
+            ' Interviewing new employers,<br>' +
+            ' Long-term strategy of delivery,<br>' +
+            ' Communicating with investor representatives,<br>' +
             ' Generating new ideas',
         'footerResult': ''
     }, {
         'headerTitle': 'Clubberspot',
         'header': 'Clubberspot - project which helps to provide safe organization of concerts between venues and musicants from 1 side, and free newsletter for providing information about entertaining events from other side ',
-        'body': 'Team: 4 members of backend team, 3 members of frontend team, 2 members of mobile team, 1 member - infrastructure, 3 designers, 10 copywriters' +
-            'MoreInfo: https://clubberspot.com/' +
-            'Role: CTO',
-        'footerList': 'Architecture.' +
-            'BackEnd core features development.' +
-            'Infrastructure deployment' +
-            'Troubleshooting. ' +
+        'body': '<b>Team</b> 4 members of backend team, 3 members of frontend team, 2 members of mobile team, 1 member - infrastructure, 3 designers, 10 copywriters<br>' +
+            '<b>MoreInfo</b> https://clubberspot.com/<br>' +
+            '<b>Role</b> CTO',
+        'footerList': 'Architecture.<br>' +
+            'BackEnd core features development.<br>' +
+            'Infrastructure deployment<br>' +
+            'Troubleshooting.<br>' +
             'Preparing technical side of presentations for investors',
         'footerResult': ''
     }, {
         'headerTitle': 'Sklad#1',
         'header': 'Sklad#1 - marketplace for tires which is working for all countries in CIS. Consists of 3 parts: backend, frontend, analytic engine',
-        'body': 'Team: 4 developers' +
-            'Role: Architect',
-        'footerList': 'Architecture.' +
-            'BackEnd core features development.' +
-            'Technical consultations.' +
-            'Troubleshooting. ' +
+        'body': '<b>Team</b> 4 developers<br>' +
+            '<b>Role</b> Architect',
+        'footerList': 'Architecture.<br>' +
+            'BackEnd core features development.<br>' +
+            'Technical consultations.<br>' +
+            'Troubleshooting.<br>' +
             'Performance Improvement',
-        'footerResult': 'MoreInfo: https://skladn1.com/'
+        'footerResult': '<b>MoreInfo</b> https://skladn1.com/'
     }, {
         'headerTitle': 'Gran-Call',
         'header': 'Gran-Call contains internal system for supporting call-center and helps to exchange information between 40_000 users per second.',
-        'body': 'Team: 7 developers' +
-            'Role: Senior Software Engineer/ DevOps',
-        'footerList': 'Architecture/Infrastructure.' +
-            'Migration to the Cloud.' +
+        'body': '<b>Team</b> 7 developers<br>' +
+            '<b>Role</b> Senior Software Engineer/ DevOps',
+        'footerList': 'Architecture/Infrastructure.<br>' +
+            'Migration to the Cloud.<br>' +
             'Technical consultations.',
-        'footerResult': 'MoreInfo: http://www.gran-call.ru/'
+        'footerResult': '<b>MoreInfo</b> http://www.gran-call.ru/'
     }, {
         'headerTitle': 'Messaging app with blockchain wallet',
         'header': 'Messaging app with blockchain wallet, location based search of nearby users, Bluetooth and web support',
-        'body': 'Team: 2 mobile developers ' +
-            'Language: Kotlin' +
-            'Technologies: Android SDK, Coroutines, Room, Koin ' +
-            'Platform: Android/iOS' +
-            'Role: Core developers',
-        'footerList': 'Architecture.' +
-            'XMPP messaging.' +
+        'body': '<b>Team</b> 2 mobile developers<br>' +
+            '<b>Language</b> Kotlin<br>' +
+            '<b>Technologies</b> Android SDK, Coroutines, Room, Koin<br>' +
+            '<b>Platform</b> Android/iOS<br>' +
+            '<b>Role</b> Core developers',
+        'footerList': 'Architecture.<br>' +
+            'XMPP messaging.<br>' +
             'Bluetooth messaging.',
         'footerResult': ''
     }, {
         'headerTitle': 'Corporate messaging platform',
         'header': 'Corporate messaging platform based on gRPC. Messaging features, voice and video calls, file and location sharing, independent corporate server',
-        'body': 'Team: 2 mobile developers ' +
-            'Language: Kotlin' +
-            'Technologies: gRPC, RX Android, Room, Koin' +
-            'Platform: Android/iOS' +
-            'Role: Core developers',
-        'footerList': 'Security features.' +
+        'body': '<b>Team</b> 2 mobile developers<br>' +
+            '<b>Language</b> Kotlin<br>' +
+            '<b>Technologies</b> gRPC, RX Android, Room, Koin<br>' +
+            '<b>Platform</b> Android/iOS<br>' +
+            '<b>Role</b> Core developers',
+        'footerList': 'Security features.<br>' +
             'File sharing.',
         'footerResult': ''
     }, {
         'headerTitle': 'Mobile client for private taxi service',
         'header': 'Rider/Driver apps, food delivery, navigation',
-        'body': 'Team: 5 mobile developers ' +
-            'Language: Java' +
-            'Technologies: RX Android, SQLite, Eventbus, Google Maps Api' +
-            'Platform: Android' +
-            'Role: Core developers',
-        'footerList': 'Architecture.' +
+        'body': '<b>Team</b> 5 mobile developers<br>' +
+            '<b>Language</b> Java<br>' +
+            '<b>Technologies</b> RX Android, SQLite, Eventbus, Google Maps Api<br>' +
+            '<b>Platform</b> Android<br>' +
+            '<b>Role</b> Core developers',
+        'footerList': 'Architecture.<br>' +
             'Implementing core functionality',
         'footerResult': ''
     }, {
         'headerTitle': ' App for managing smart home ecosystem',
         'header': ' App for managing smart home ecosystem including thermostats, sensors, smart bulbs, etc.',
-        'body': 'Team: 5 mobile developers ' +
-            'Language: Java' +
-            'Technologies: RX Android, Dagger 2, Mockito' +
-            'Platform: Android' +
-            'Role: Core developers',
-        'footerList': 'Full refactoring of old app' +
+        'body': '<b>Team</b> 5 mobile developers<br>' +
+            '<b>Language</b> Java<br>' +
+            '<b>Technologies</b> RX Android, Dagger 2, Mockito<br>' +
+            '<b>Platform</b> Android<br>' +
+            '<b>Role</b> Core developers',
+        'footerList': 'Full refactoring of old app<br>' +
             'Support of new devices',
         'footerResult': ''
     }
 ];
 
-const card = ({ headerTitle, header, body, footerList, footerResult }) => `
-<div class="col-lg-4 col-md-6 col-sm-12 mt-4 align-items-stretchs d-flex">
+const processBody = (body) => {
+    return body;
+}
+
+const card = ({ headerTitle, header, body, footerList, footerResult }) => {
+
+    return `<div class="col-lg-4 col-md-6 col-sm-12 mt-4 align-items-stretchs d-flex">
+
 <div class="card">
 <div class="card-header flex-fill">
     <h4 class="card-title text-center">${headerTitle}</h4>
@@ -194,7 +200,7 @@ const card = ({ headerTitle, header, body, footerList, footerResult }) => `
 
 <div class="card-body flex-fill">
     <p class="card-text">
-    ${body}
+    ${processBody(body)}
     </p>
 </div>
 <div class="card-footer flex-fill">
@@ -203,10 +209,11 @@ const card = ({ headerTitle, header, body, footerList, footerResult }) => `
 </div>
 </div>
 </div>
-`;
+`
+};
 
 $(document).ready(function() {
-    $('#cards').html(cardsArray.map(card).join('\n'));
+    $('#cards').html(cardsArray.map(card).join('<br>'));
 });
 
 $(document).ready(function() {

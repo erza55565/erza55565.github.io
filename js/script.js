eed90030e7f786a95a243ef90870ad42bfe40bcb
Koval Yelyzaveta <elizaveta55565@gmail.com>
2020-09-03 16:04:43 +0300

    change the design of cards, add slogan and Max's bio, change input

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 let cardsArray = [{
         'headerTitle': 'Airlines online booking',
         'header': 'Implementation of a couple microservices for the airlines booking system. Booking tickets, processing flights, routing, prices, discounts, etc. Also, the project had the internal layers of system services for big data processing such as recalculation prices, flight shifting, optimization, etc. ',
-        'body': '<b>Team:</b> 3 backend, 2 frontend, 1 devops.<br>' +
-            '<b>Language:</b> Java / Scala<br>' +
-            '<b>Technologies:</b> Docker, Spark, RabbitMQ, Cassandra<br>' +
-            '<b>Platform:</b> Cross-platform<br>' +
-            '<b>Role:</b> Architect / Core developer ',
+        'team': '3 backend, 2 frontend, 1 devops',
+        'language': 'Java, Scala',
+        'technologies': 'Docker, Spark, RabbitMQ, Cassandra',
+        'platform': 'Cross-platform',
+        'role': 'Architect / Core developer',
         'footerList': '<li>Architecture.</li>' +
             '<li>BackEnd core features development.</li>' +
             '<li>Technical consultations.</li>' +
@@ -13,16 +13,28 @@ let cardsArray = [{
             '<li>Interviewing new employers.</li>' +
             '<li>Solving difficult technical issues.</li>',
         'footerResult': 'The customer has improved speed of transaction processing (booking, searching, etc) Optimization of logistics and fees has been implemented. '
+    }, {
+        'headerTitle': 'Clubberspot',
+        'header': 'Clubberspot - project which helps to provide safe organization of concerts between venues and musicants from 1 side, and free newsletter for providing information about entertaining events from other side ',
+        'team': '4 members of backend team, 3 members of frontend team, 2 members of mobile team, 1 member - infrastructure, 3 designers, 10 copywriters',
+        'moreInfo': '<a href="https://clubberspot.com/">https://clubberspot.com/</a>',
+        'role': 'CTO',
+        'footerList': '<li>Architecture.</li>' +
+            '<li>BackEnd core features development.</li>' +
+            '<li>Infrastructure deployment</li>' +
+            '<li>Troubleshooting.</li>' +
+            '<li>Preparing technical side of presentations for investors</li>',
+        'footerResult': ''
     },
     {
         'headerTitle': 'Payment banking based on blockchain infrastructure',
         'header': 'Research and investigation which blockchain network will be the best for specific payment transaction. The main idea is comparing, analyze, and select appropriate blockchain network for the specific business transaction. The goal was transaction size, time and confirmation.',
-        'body': '<b>Team:</b> 3 backend, 3 frontend, 2 devops.<br>' +
-            '<b>MoreInfo:</b> <a href="https://github.com/imaxfp/blockchain-workbench-rest">https://github.com/imaxfp/blockchain-workbench-rest</a> <br>' +
-            '<b>Language:</b> Java / Scala<br>' +
-            '<b>Technologies:</b> Docker, r3Corda, Hyperledger, Ethereum.<br>' +
-            '<b>Platform:</b> Cross-platform<br>' +
-            '<b>Role:</b> Architect / Core developer',
+        'team': '3 backend, 3 frontend, 2 devops',
+        'moreInfo': '<a href="https://github.com/imaxfp/blockchain-workbench-rest">https://github.com/imaxfp/blockchain-workbench-rest</a> ',
+        'language': 'Java, Scala',
+        'technologies': 'Docker, r3Corda, Hyperledger, Ethereum.',
+        'platform': 'Cross-platform',
+        'role': 'Architect / Core developer',
         'footerList': '<li>Architecture.</li>' +
             '<li>BackEnd core features development.</li>' +
             '<li>Technical consultations.</li>' +
@@ -34,11 +46,11 @@ let cardsArray = [{
     {
         'headerTitle': 'Supply chain solution for cargo delivery',
         'header': 'The main idea of the project is solving trust issue between businesses and merchants. Exclude human errors and proxies agents. Speed up transaction processing. ',
-        'body': '<b>Team:</b> 5 backend, 4 frontend, 2 devops.<br>' +
-            '<b>Language:</b> Scala<br>' +
-            '<b>Technologies:</b> Docker, r3Corda<br>' +
-            '<b>Platform:</b> Cross-platform<br>' +
-            '<b>Role:</b> Architect / Core developer',
+        'team': '5 backend, 4 frontend, 2 devops',
+        'language': 'Scala',
+        'technologies': 'Docker, r3Corda',
+        'platform': 'Cross-platform',
+        'role': 'Architect / Core developer',
         'footerList': '<li>Architecture.</li>' +
             '<li>BackEnd core features development.</li>' +
             '<li>Technical consultations.</li>' +
@@ -49,11 +61,11 @@ let cardsArray = [{
     }, {
         'headerTitle': 'Prediction and searching financial instruments based on ML/AI algorithms',
         'header': 'ML based solution for searching similarities of different trading securities, bonds, etc. The main goal of the project - analyzing, classification and select closest financial instruments by the selected features. The project use mixed approach wit supervised and unsupervised learning. ',
-        'body': '<b>Team:</b> 4 backend, 2 frontend, 1 devops.<br>' +
-            '<b>Language:</b> Python, Java<br>' +
-            '<b>Technologies and libs:</b> <a href="https://matplotlib.org">https://matplotlib.org</a>,  <a href="https://scikit-learn.org">https://scikit-learn.org</a>, <a href="https://www.docker.com">https://www.docker.com</a>, <a href="https://pandas.pydata.org">https://pandas.pydata.org</a><br>' +
-            '<b>Platform:</b> Cross-platform<br>' +
-            '<b>Role:</b> Data scientist / Core developer ',
+        'team': '4 backend, 2 frontend, 1 devops',
+        'language': 'Python, Java',
+        'links': '<a href="https://matplotlib.org">https://matplotlib.org</a>,  <a href="https://scikit-learn.org">https://scikit-learn.org</a>, <a href="https://www.docker.com">https://www.docker.com</a>, <a href="https://pandas.pydata.org">https://pandas.pydata.org</a>',
+        'platform': 'Cross-platform',
+        'role': 'Data scientist / Core developer ',
         'footerList': '<li>Data analyzing.</li>' +
             '<li>Architecture.</li>' +
             '<li>BackEnd core features development.</li>' +
@@ -63,11 +75,11 @@ let cardsArray = [{
     }, {
         'headerTitle': 'ML/AI based solution for prediction of volatility financial instruments',
         'header': 'The main idea is tracking selected financial indexes and features. Provide unsupervised cauterization algorithms and supervised learning based on big financial datasets.',
-        'body': '<b>Team:</b> 4 backend, 2 frontend, 1 devops.<br>' +
-            '<b>Language:</b> Python, Java<br>' +
-            '<b>Technologies and libs:</b> <a href="https://matplotlib.org">https://matplotlib.org</a>,  <a href="https://scikit-learn.org">https://scikit-learn.org</a>, <a href="https://www.docker.com">https://www.docker.com</a>, <a href="https://pandas.pydata.org">https://pandas.pydata.org</a><br>' +
-            '<b>Platform:</b> Cross-platform<br>' +
-            '<b>Role:</b> Data scientist / Core developer ',
+        'team': '4 backend, 2 frontend, 1 devops',
+        'language': 'Python, Java',
+        'links': '<a href="https://matplotlib.org">https://matplotlib.org</a>,  <a href="https://scikit-learn.org">https://scikit-learn.org</a>, <a href="https://www.docker.com">https://www.docker.com</a>, <a href="https://pandas.pydata.org">https://pandas.pydata.org</a>',
+        'platform': 'Cross-platform',
+        'role': 'Data scientist / Core developer ',
         'footerList': '<li>Data analyzing.</li>' +
             '<li>Architecture.</li>' +
             '<li>BackEnd core features development.</li>' +
@@ -77,11 +89,11 @@ let cardsArray = [{
     }, {
         'headerTitle': 'The project was connected with financial and trading industry',
         'header': 'The main idea is clusterization and finding similarities between trading stocks.',
-        'body': '<b>Team:</b> 5 backend, 4 frontend, 2 devops.<br>' +
-            '<b>Language:</b> Python / Java<br>' +
-            '<b>Technologies:</b> Docker, spring boot, hibernate,  sklearn, pandas, matplotlib, etc<br>' +
-            '<b>Platform:</b> Cross-platform<br>' +
-            '<b>Role:</b> Architect / Core developer ',
+        'team': '5 backend, 4 frontend, 2 devops',
+        'language': 'Python, Java',
+        'technologies': 'Docker, spring boot, hibernate,  sklearn, pandas, matplotlib, etc',
+        'platform': 'Cross-platform',
+        'role': 'Architect / Core developer ',
         'footerList': '<li>Architecture and development.</li>' +
             '<li>Data analyzes.</li>' +
             '<li>Applying ML algorithms.</li>' +
@@ -91,10 +103,10 @@ let cardsArray = [{
     }, {
         'headerTitle': '',
         'header': '',
-        'body': '<b>Language:</b> Scala<br>' +
-            '<b>Technologies:</b> r3Corda, Java, Docker, Postgres, Akka, Spring, ReactJS<br>' +
-            '<b>Platform:</b> Cross-platform<br>' +
-            '<b>Role:</b> CTO ',
+        'language': 'Scala',
+        'technologies': 'r3Corda, Java, Docker, Postgres, Akka, Spring, ReactJS',
+        'platform': 'Cross-platform',
+        'role': 'CTO ',
         'footerList': '<li>Architecture.</li>' +
             '<li>BackEnd, FrontEnd, Infrastructure organization.</li>' +
             '<li>Saving human resources(avoiding burnout in fast-changeable environment with possibility to work under pressure, etc).</li>' +
@@ -105,45 +117,33 @@ let cardsArray = [{
             '<li>Generating new ideas.</li>',
         'footerResult': ''
     }, {
-        'headerTitle': 'Clubberspot',
-        'header': 'Clubberspot - project which helps to provide safe organization of concerts between venues and musicants from 1 side, and free newsletter for providing information about entertaining events from other side ',
-        'body': '<b>Team:</b> 4 members of backend team, 3 members of frontend team, 2 members of mobile team, 1 member - infrastructure, 3 designers, 10 copywriters<br>' +
-            '<b>MoreInfo:</b> <a href="https://clubberspot.com/">https://clubberspot.com/</a><br>' +
-            '<b>Role:</b> CTO',
-        'footerList': '<li>Architecture.</li>' +
-            '<li>BackEnd core features development.</li>' +
-            '<li>Infrastructure deployment</li>' +
-            '<li>Troubleshooting.</li>' +
-            '<li>Preparing technical side of presentations for investors</li>',
-        'footerResult': ''
-    }, {
         'headerTitle': 'Sklad#1',
         'header': 'Sklad#1 - marketplace for tires which is working for all countries in CIS. Consists of 3 parts: backend, frontend, analytic engine',
-        'body': '<b>Team:</b> 4 developers<br>' +
-            '<b>Role:</b> Architect',
+        'team': '4 developers',
+        'role': 'Architect',
         'footerList': '<li>Architecture.</li>' +
             '<li>BackEnd core features development.</li>' +
             '<li>Technical consultations.</li>' +
             '<li>Troubleshooting.</li>' +
             '<li>Performance Improvement.</li>',
-        'footerResult': '<b>MoreInfo:</b> <a href="https://skladn1.com/">https://skladn1.com/</a>'
+        'footerResult': 'MoreInfo : <a href="https://skladn1.com/">https://skladn1.com/</a>'
     }, {
         'headerTitle': 'Gran-Call',
         'header': 'Gran-Call contains internal system for supporting call-center and helps to exchange information between 40_000 users per second.',
-        'body': '<b>Team:</b> 7 developers<br>' +
-            '<b>Role:</b> Senior Software Engineer/ DevOps',
+        'team': '7 developers',
+        'role': 'Senior Software Engineer/ DevOps',
         'footerList': '<li>Architecture/Infrastructure.</li>' +
             '<li>Migration to the Cloud.</li>' +
             '<li>Technical consultations.</li>',
-        'footerResult': '<b>MoreInfo:</b> <a href="http://www.gran-call.ru/">http://www.gran-call.ru/</a>'
+        'footerResult': 'MoreInfo: <a href="http://www.gran-call.ru/">http://www.gran-call.ru/</a>'
     }, {
         'headerTitle': 'Messaging app with blockchain wallet',
         'header': 'Messaging app with blockchain wallet, location based search of nearby users, Bluetooth and web support',
-        'body': '<b>Team:</b> 2 mobile developers<br>' +
-            '<b>Language:</b> Kotlin<br>' +
-            '<b>Technologies:</b> Android SDK, Coroutines, Room, Koin<br>' +
-            '<b>Platform:</b> Android/iOS<br>' +
-            '<b>Role:</b> Core developers',
+        'team': '2 mobile developers',
+        'language': 'Kotlin',
+        'technologies': 'Android SDK, Coroutines, Room, Koin',
+        'platform': 'Android/iOS',
+        'role': 'Core developers',
         'footerList': '<li>Architecture.</li>' +
             '<li>XMPP messaging.</li>' +
             '<li>Bluetooth messaging.</li>',
@@ -151,61 +151,84 @@ let cardsArray = [{
     }, {
         'headerTitle': 'Corporate messaging platform',
         'header': 'Corporate messaging platform based on gRPC. Messaging features, voice and video calls, file and location sharing, independent corporate server',
-        'body': '<b>Team:</b> 2 mobile developers<br>' +
-            '<b>Language:</b> Kotlin<br>' +
-            '<b>Technologies:</b> gRPC, RX Android, Room, Koin<br>' +
-            '<b>Platform:</b> Android/iOS<br>' +
-            '<b>Role:</b> Core developers',
+        'team': '2 mobile developers',
+        'language': 'Kotlin',
+        'technologies': 'gRPC, RX Android, Room, Koin',
+        'platform': 'Android/iOS',
+        'role': 'Core developers',
         'footerList': '<li>Security features.</li>' +
             '<li>File sharing.</li>',
         'footerResult': ''
     }, {
         'headerTitle': 'Mobile client for private taxi service',
         'header': 'Rider/Driver apps, food delivery, navigation',
-        'body': '<b>Team:</b> 5 mobile developers<br>' +
-            '<b>Language:</b> Java<br>' +
-            '<b>Technologies:</b> RX Android, SQLite, Eventbus, Google Maps Api<br>' +
-            '<b>Platform:</b> Android<br>' +
-            '<b>Role:</b> Core developers',
+        'team': '5 mobile developers',
+        'language': 'Java',
+        'technologies': 'RX Android, SQLite, Eventbus, Google Maps Api',
+        'platform': 'Android',
+        'role': 'Core developers',
         'footerList': '<li>Architecture.</li>' +
             '<li>Implementing core functionality.</li>',
         'footerResult': ''
     }, {
-        'headerTitle': ' App for managing smart home ecosystem',
-        'header': ' App for managing smart home ecosystem including thermostats, sensors, smart bulbs, etc.',
-        'body': '<b>Team:</b> 5 mobile developers<br>' +
-            '<b>Language:</b> Java<br>' +
-            '<b>Technologies:</b> RX Android, Dagger 2, Mockito<br>' +
-            '<b>Platform:</b> Android<br>' +
-            '<b>Role:</b> Core developers',
+        'headerTitle': 'App for managing smart home ecosystem',
+        'header': 'App for managing smart home ecosystem including thermostats, sensors, smart bulbs, etc.',
+        'team': '5 mobile developers',
+        'language': 'Java',
+        'technologies': 'RX Android, Dagger 2, Mockito',
+        'platform': 'Android',
+        'role': 'Core developers',
         'footerList': '<li>Full refactoring of old app.</li>' +
             '<li>Support of new devices.</li>',
         'footerResult': ''
     }
 ];
 
-const processBody = (body) => {
+let button = (tag, iconClass) => `<button class="btn mr-3 mb-3" type="button" disabled><i class="fas ${iconClass}"></i>${tag}</button>`;
+let teamTag = (tag) => button(tag, "fa-user-friends");
+let languageTag = (tag) => button(tag, "fa-cog px-1");
+let technologiesTag = (tag) => button(tag, "fa-cog px-1");
+let platformTag = (tag) => button(tag, "fa-laptop");
+let roleTag = (tag) => button(tag, "fa-user-cog");
+let buildTag = (tag, tagFunction) => {
+    if (!tag) {
+        return '';
+    }
+    return tag.split(',').map(tagFunction).join('');
+}
+
+const buildBody = (team, language, technologies, platform, role) => {
+    let body = '';
+    body = body + buildTag(team, teamTag);
+    body = body + buildTag(language, languageTag);
+    body = body + buildTag(technologies, technologiesTag);
+    body = body + buildTag(platform, platformTag);
+    body = body + buildTag(role, roleTag);
     return body;
 }
 
-const card = ({ headerTitle, header, body, footerList, footerResult }) => {
+const card = ({ headerTitle, header, team, language, technologies, platform, role, footerList, footerResult }) => {
 
-    return `<div class="col-lg-4 col-md-6 col-sm-12 mt-4 align-items-stretchs d-flex">
+    let body = buildBody(team, language, technologies, platform, role);
 
+    return `<div class="col-lg-6 col-md-6 col-sm-12 mt-4 align-items-stretchs d-flex">
 <div class="card">
 <div class="card-header flex-fill">
     <h4 class="card-title text-center">${headerTitle}</h4>
-    <p class="card-text">${header}</p>
+    <p class="card-text text-justify">${header}</p>
 </div>
-
 <div class="card-body flex-fill">
-    <p class="card-text">
-    ${processBody(body)}
+    <p class="card-text text-center">
+    ${body}
     </p>
+    <button class="btn content_toggle mx-auto btn-block flex-fill" type="button">
+        Tasks performed<i class="fas fa-chevron-down pl-2" id="chevron-icon"></i>
+    </button>
 </div>
-<div class="card-footer flex-fill">
+<div class="card-footer text-justify flex-fill" style="display: none;">
     ${footerList}
     ${footerResult}
+</div>
 </div>
 </div>
 </div>
@@ -218,6 +241,16 @@ $(document).ready(function() {
 
 
 $(document).ready(function() {
-    $('#cards').prepend(cardsArray.slice(0, 3).map(card).join(' '));
-    $('#collapseCards').html(cardsArray.slice(3, cardsArray.length).map(card).join(' '));
+    $('#cards').prepend(cardsArray.slice(0, 2).map(card).join(' '));
+    $('#collapseCards').html(cardsArray.slice(2, cardsArray.length).map(card).join(' '));
+});
+
+$(document).ready(function() {
+    $('.content_toggle').each(function() {
+        $(this).click(function() {
+            $('.card-footer').slideToggle(300);
+            $('#chevron-icon', this).toggleClass('fa-chevron-down fa-chevron-up');
+            return false;
+        });
+    })
 });
